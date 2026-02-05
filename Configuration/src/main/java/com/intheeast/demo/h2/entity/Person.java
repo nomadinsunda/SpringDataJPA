a6ee229ba1cd8cfed27d93173525a4c962537e9a
Sungwon Seo <intheeast0305@gmail.com>
2026-02-05 15:06:29 +0900

    update...

--- Configuration/src/main/java/com/intheeast/demo/h2/entity/Person.java
+++ Configuration/src/main/java/com/intheeast/demo/h2/entity/Person.java
@@ -2,11 +2,14 @@ package com.intheeast.demo.h2.entity;
 
 import org.hibernate.annotations.DynamicInsert;
 
+import jakarta.persistence.Column;
 import jakarta.persistence.Entity;
 import jakarta.persistence.GeneratedValue;
 import jakarta.persistence.GenerationType;
 import jakarta.persistence.Id;
 import jakarta.persistence.Transient;
+import jakarta.validation.constraints.Email;
+import jakarta.validation.constraints.NotEmpty;
 import lombok.AllArgsConstructor;
 import lombok.Getter;
 import lombok.NoArgsConstructor;
@@ -26,6 +29,11 @@ public class Person {
 	
 	private String firstname;
 	private String lastname;
+	
+	@Column(unique = true)
+    @NotEmpty(message = "Email cannot be empty")
+    @Email(message = "Invalid email format")
+    private String email;
 	
 	@Transient
 	private String fullname;
